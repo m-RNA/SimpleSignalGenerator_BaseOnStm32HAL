b8b7fe410172048a7868b21a6af76eae0923f4c3
m-RNA <m-RNA@qq.com>
2022-06-19 21:29:22 +0800

    串口命令更加灵活

--- Project/BLL/bll_setting_by_uart_receive_command.c
+++ Project/BLL/bll_setting_by_uart_receive_command.c
@@ -43,22 +43,29 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 
 void UART_Check_Rx_Command_Task(void)
 {
-    u16 temp_mode, temp_freq, temp_vpp;
+    u16 i;
+    u32 temp_freq = 0;
+    u16 temp_mode, temp_vpp;
+    
     if(!Rx_Index) return;
     if(SW_Timer_Tick > BSP_GetTick()) return; // ※ return 用 > 
     
-    if(Rx_Index == 11) // {0:020,3.0}
+    if(Rx_Index <= 14) // {0:20,3.0}
     {
         // 检查命令格式是否正确
-        if((Rx_Buffer[0] != '{') && (Rx_Buffer[2] != ':') && (Rx_Buffer[6] != ',') && (Rx_Buffer[8] != '.')&& (Rx_Buffer[10] != '}'))
+        if((Rx_Buffer[0] != '{') && (Rx_Buffer[2] != ':') && (Rx_Buffer[Rx_Index - 5] != ',') && (Rx_Buffer[Rx_Index - 3] != '.')&& (Rx_Buffer[Rx_Index - 1] != '}'))
             goto Send_Error;
 
         // 检查命令数据范围是否合理
         temp_mode =  Rx_Buffer[1] - '0';
-        temp_freq = (Rx_Buffer[3] - '0') * 100 + (Rx_Buffer[4] - '0') * 10 + Rx_Buffer[5] - '0';
-        temp_vpp  = (Rx_Buffer[7] - '0') * 10+ Rx_Buffer[9] - '0';
+        temp_vpp  = (Rx_Buffer[Rx_Index - 4] - '0') * 10+ Rx_Buffer[Rx_Index - 2] - '0';
+        for(i = 3; i < Rx_Index - 5; i++)
+        {
+            temp_freq*=10;
+            temp_freq+=(Rx_Buffer[i] - '0');
+        }
         if(temp_mode >= 5) goto Send_Error;
-        if(temp_freq > 90) goto Send_Error;
+        if(temp_freq > 100000) goto Send_Error;
         if((temp_vpp  > 33) || (temp_vpp  < 15)) goto Send_Error;
         
         // 命令正确
@@ -102,7 +109,7 @@ void UART_Check_Rx_Command_Task(void)
     }
 
     // 清零接收缓冲
+    memset(Rx_Buffer, 0, Rx_Index);
     Rx_Index = 0;
-    memset(Rx_Buffer, 0, RX_MAX_LENTH);
 }
 
