0771a72353430ada80c2b23d7c5a9ac4f99e9499
m-RNA <m-RNA@qq.com>
2022-06-20 22:23:59 +0800

    可以配置使用的片上资源

--- Project/BSP/Inc/bsp.h
+++ Project/BSP/Inc/bsp.h
@@ -2,15 +2,22 @@
 #define __BSP_H_
 
 #include "main.h"
+#include "tim.h"
+#include "dac.h"
+#include "opamp.h"
+
 #include "bsp_lcd.h"
 #include "bsp_led.h"
 #include "bsp_key.h"
 #include "bsp_adc.h"
 #include "bsp_beep.h"
+#include "bsp_tim.h"
 #include "bsp_dac.h"
 #include "bsp_uart.h"
 
 #define BSP_GetTick() HAL_GetTick()
 
-#endif
+#define DAC_MOUDLE       hdac3
+#define DAC_Tiggle_Timer htim2
 
+#endif
